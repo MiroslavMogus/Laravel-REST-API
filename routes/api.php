ba805b68aeccbf1cd78eab70cbc807b24118061b
vagrant <vagrant@homestead>
2017-05-28 10:52:49 +0200

    After Routes Creation

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,38 @@ use Illuminate\Http\Request;
 |
 */
 
+/**
+ * Buyers
+ */
 
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index','show']]);
+
+/**
+ * Sellers
+ */
+
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index','show']]);
+
+/**
+ * Categories
+ */
+
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create','edit']]);
+
+/**
+ * Transactions
+ */
+
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index','show']]);
+
+/**
+ * Products
+ */
+
+Route::resource('products', 'Product\ProductController', ['only' => ['index','show']]);
+
+/**
+ * Users
+ */
+
+Route::resource('users', 'User\UserController', ['except' => ['create','edit']]);
